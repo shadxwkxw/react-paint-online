f23fc7ee9c776efe2eb226f2b489161b85533b2d
shadxwkxw <lolkek2468@gmail.com>
2025-06-11 18:36:47 +0300

    --init commit

---
+++ paint-online/src/components/SettingBar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import '../styles/toolbar.scss'
+
+const SettingBar = () => {
+    return (
+        <div className="settingBar">
+
+        </div>
+    )
+}
+
+export default SettingBar
---
+++ paint-online/src/components/Toolbar.jsx
@@ -0,0 +1,12 @@
+import React from "react";
+import '../styles/toolbar.scss'
+
+const Toolbar = () => {
+    return (
+        <div className="toolbar">
+            <button className="toolbar__btn brush"></button>
+        </div>
+    )
+}
+
+export default Toolbar
