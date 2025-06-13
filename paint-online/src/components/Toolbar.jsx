519a6a96aee88118d7550de784dce431bbd48921
shadxwkxw <lolkek2468@gmail.com>
2025-06-14 02:50:51 +0300

    --added drawing with a circle and a rect

--- paint-online/src/components/Toolbar.jsx
+++ paint-online/src/components/Toolbar.jsx
@@ -2,14 +2,16 @@ import React from "react";
 import '../styles/toolbar.scss'
 import toolState from "../store/toolState";
 import Brush from "../tools/Brush";
+import Rect from "../tools/Rect";
 import canvasState from "../store/canvasState";
+import Circle from "../tools/Circle";
 
 const Toolbar = () => {
     return (
         <div className="toolbar">
             <button className="toolbar__btn brush" onClick={() => toolState.setTool(new Brush(canvasState.canvas))}></button>
-            <button className="toolbar__btn rect"></button>
-            <button className="toolbar__btn circle"></button>
+            <button className="toolbar__btn rect" onClick={() => toolState.setTool(new Rect(canvasState.canvas))}></button>
+            <button className="toolbar__btn circle" onClick={() => toolState.setTool(new Circle(canvasState.canvas))}></button>
             <button className="toolbar__btn eraser"></button>
             <button className="toolbar__btn line"></button>
             <input style={{marginLeft: 10}} type="color" />
