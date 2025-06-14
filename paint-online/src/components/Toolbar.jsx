a60baaeb81f07f0df06a6c8e850362c9df2cbb4e
shadxwkxw <lolkek2468@gmail.com>
2025-06-14 03:19:09 +0300

    --added drawing with a eraser and a line

--- paint-online/src/components/Toolbar.jsx
+++ paint-online/src/components/Toolbar.jsx
@@ -5,6 +5,8 @@ import Brush from "../tools/Brush";
 import Rect from "../tools/Rect";
 import canvasState from "../store/canvasState";
 import Circle from "../tools/Circle";
+import Eraser from "../tools/Eraser";
+import Line from "../tools/Line";
 
 const Toolbar = () => {
     return (
@@ -12,8 +14,8 @@ const Toolbar = () => {
             <button className="toolbar__btn brush" onClick={() => toolState.setTool(new Brush(canvasState.canvas))}></button>
             <button className="toolbar__btn rect" onClick={() => toolState.setTool(new Rect(canvasState.canvas))}></button>
             <button className="toolbar__btn circle" onClick={() => toolState.setTool(new Circle(canvasState.canvas))}></button>
-            <button className="toolbar__btn eraser"></button>
-            <button className="toolbar__btn line"></button>
+            <button className="toolbar__btn eraser" onClick={() => toolState.setTool(new Eraser(canvasState.canvas))}></button>
+            <button className="toolbar__btn line" onClick={() => toolState.setTool(new Line(canvasState.canvas))}></button>
             <input style={{marginLeft: 10}} type="color" />
             <button className="toolbar__btn undo"></button>
             <button className="toolbar__btn redo"></button>
